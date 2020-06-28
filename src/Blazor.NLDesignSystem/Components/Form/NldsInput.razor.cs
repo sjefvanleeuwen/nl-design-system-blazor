56c33dc6b384b0fb6ded29f9d9416bf34e22bf8a
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-28 09:42:58 +0200

    Input Control

---
+++ src/Blazor.NLDesignSystem/Components/Form/NldsInput.razor.cs
@@ -0,0 +1,29 @@
+﻿using Blazor.NLDesignSystem.Extensions;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.NLDesignSystem.Components.Form
+{
+    public partial class NldsInput
+    {
+        [Parameter]
+        public string Id { get; set; }
+        [Parameter]
+        public bool IsInvalid { get; set; }
+        [Parameter]
+        public LabelAlignment LabelAlignment { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public RenderFragment Hint { get; set; }
+        [Parameter]
+        public RenderFragment Error { get; set; }
+        [Parameter]
+        public InputSize Size { get; set; }
+        [Parameter]
+        public string AriaDescribedBy { get; set; }
+        [Parameter]
+        public bool IsRequired { get; set; }
+        private string labelAlignment => LabelAlignment.GetDescription<StyleAttribute>();
+        private string size => Size.GetDescription<StyleAttribute>();
+    }
+}
