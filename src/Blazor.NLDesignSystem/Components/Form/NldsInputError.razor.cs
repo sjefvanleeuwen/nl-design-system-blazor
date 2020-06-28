e827f2e61bf4ac084c4b66f354772df7a39a5013
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-28 09:43:10 +0200

    Input error control

---
+++ src/Blazor.NLDesignSystem/Components/Form/NldsInputError.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.NLDesignSystem.Components.Form
+{
+    public partial class NldsInputError
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
