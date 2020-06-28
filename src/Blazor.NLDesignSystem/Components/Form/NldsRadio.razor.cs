1ac70dc279809b2ec52c280ce26f131aaf24cdad
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-28 21:02:10 +0200

    First version of Radio buttons

---
+++ src/Blazor.NLDesignSystem/Components/Form/NldsRadio.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Collections.Generic;
+
+namespace Blazor.NLDesignSystem.Components.Form
+{
+    public partial class NldsRadio
+    {
+        [Parameter]
+        public string Group { get; set; }
+        [Parameter]
+        public string Legend { get; set; }
+        [Parameter]
+        public List<RadioItem> Items { get; set; }
+
+        [Parameter]
+        public RadioItem CurrentItem { get; set; }
+
+        [Parameter]
+        public EventCallback<NldsRadio> OnChange { get; set; }
+
+        private async void onChange(RadioItem item)
+        {
+            CurrentItem = item;
+            if (OnChange.HasDelegate)
+            {
+                await OnChange.InvokeAsync(this);
+            }
+
+        }
+    }
+
+    public class RadioItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
