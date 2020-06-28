640a94b023943c9587d230d08d6af5ce9a240fa1
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-28 09:43:20 +0200

    Input Hint Control

---
+++ src/Blazor.NLDesignSystem/Components/Form/NldsInputHint.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.NLDesignSystem.Components.Form
+{
+    public partial class NldsInputHint
+    {
+        [Parameter]
+        public string HintName { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
