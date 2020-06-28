afa9f37da311e0d5c256a8b86e7c74c280430ba3
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-28 09:41:46 +0200

    Input size enumeration

---
+++ src/Blazor.NLDesignSystem/Components/Base/InputSize.cs
@@ -0,0 +1,18 @@
+﻿using Blazor.NLDesignSystem.Extensions;
+
+namespace Blazor.NLDesignSystem
+{
+    public enum InputSize
+    {
+        [Style("--m")]
+        Medium,
+        [Style("--xs")]
+        ExtraSmall,
+        [Style("--s")]
+        Small,
+        [Style("--l")]
+        Large,
+        [Style("--xl")]
+        ExtraLarge
+    }
+}
