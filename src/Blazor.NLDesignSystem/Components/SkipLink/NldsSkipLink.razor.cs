2750fbfd315c75fd183408318da20488411e75a2
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-29 19:32:18 +0200

    Skiplink component

---
+++ src/Blazor.NLDesignSystem/Components/SkipLink/NldsSkipLink.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.NLDesignSystem.Components.SkipLink
+{
+    public partial class NldsSkipLink
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
