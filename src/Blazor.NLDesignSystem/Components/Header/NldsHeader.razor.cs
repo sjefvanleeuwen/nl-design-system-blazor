44e8b65c98ce7be6a3b2512a0c7010fb303c4d8f
Marijn Pessers <marijn@pessers.net>
2020-07-08 15:19:20 +0200

    Extended the Header class with extra paramaters for display options.

--- src/Blazor.NLDesignSystem/Components/Header/NldsHeader.razor.cs
+++ src/Blazor.NLDesignSystem/Components/Header/NldsHeader.razor.cs
@@ -12,5 +12,13 @@ namespace Blazor.NLDesignSystem.Components.Header
         public string Link { get; set; } = "/";
         [Parameter]
         public string LinkScreenReaderDescription { get; set; }
+        public bool IsTransparent { get; set; }
+        /// <summary>
+        /// optional extra class name for the header-logo, like header-logo--white, header-logo--cte
+        /// </summary>
+        [Parameter]
+        public string LogoClass { get; set; }
+
+        private string TransparentClass => IsTransparent ? "header--transparent" : string.Empty;
     }
 }
