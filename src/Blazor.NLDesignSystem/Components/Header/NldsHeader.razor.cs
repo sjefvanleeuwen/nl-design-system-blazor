c354f4d05078aacc4e75845847ea22efed70d46c
Marijn Pessers <marijn@pessers.net>
2020-07-14 14:23:18 +0200

    Header example added and debuggeed

--- src/Blazor.NLDesignSystem/Components/Header/NldsHeader.razor.cs
+++ src/Blazor.NLDesignSystem/Components/Header/NldsHeader.razor.cs
@@ -5,19 +5,21 @@ namespace Blazor.NLDesignSystem.Components.Header
     public partial class NldsHeader
     {
         [Parameter]
-        public RenderFragment Content { get; set; }
-        [Parameter]
         public string LinkTitle { get; set; }
         [Parameter]
         public string Link { get; set; } = "/";
         [Parameter]
         public string LinkScreenReaderDescription { get; set; }
+        [Parameter]
         public bool IsTransparent { get; set; }
         /// <summary>
         /// optional extra class name for the header-logo, like header-logo--white, header-logo--cte
         /// </summary>
         [Parameter]
         public string LogoClass { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
 
         private string TransparentClass => IsTransparent ? "header--transparent" : string.Empty;
     }
