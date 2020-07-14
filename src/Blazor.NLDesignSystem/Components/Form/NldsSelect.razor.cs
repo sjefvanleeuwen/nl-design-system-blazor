ef7ed42a853993b4baff2eee503467ea2f2c96ee
Marijn Pessers <marijn@pessers.net>
2020-07-14 11:51:54 +0200

    Select implemented

---
+++ src/Blazor.NLDesignSystem/Components/Form/NldsSelect.razor.cs
@@ -0,0 +1,76 @@
+﻿using Blazor.NLDesignSystem.Extensions;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Blazor.NLDesignSystem.Components.Form
+{
+    public partial class NldsSelect
+    {
+        /// <summary>
+        /// Optional; overrides the default value; if the input contains a hint the value "hint_" + Id will be used by default
+        ///</summary>
+        [Parameter]
+        public string AriaDescribedBy { get; set; }
+        [Parameter]
+        public string ErrorText { get; set; }
+        [Parameter]
+        public string Identifier { get; set; }
+        [Parameter]
+        public bool IsDisabled { get; set; }
+        [Parameter]
+        public bool IsRequired { get; set; }
+        [Parameter]
+        public IEnumerable<SelectItem> Items { get; set; } = new List<SelectItem>();
+        [Parameter]
+        public LabelAlignment LabelAlignment { get; set; }
+        [Parameter]
+        public InputSize Size { get; set; }
+
+        [Parameter]
+        public RenderFragment Label { get; set; }
+        [Parameter]
+        public RenderFragment Hint { get; set; }
+
+        //2-way binding
+        private string _value;
+
+        [Parameter]
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value) return;
+                _value = value;
+                ValueChanged.InvokeAsync(value);
+            }
+        }
+
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+
+        private bool IsValid => string.IsNullOrWhiteSpace(ErrorText);
+        private string LabelAlignmentStyle => LabelAlignment.GetDescription<StyleAttribute>();
+        private string SizeAppendix => Size.GetDescription<StyleAttribute>();
+
+        private IDictionary<string, object> GetAttributes()
+        {
+            var attributes = new Dictionary<string, object>();
+
+            var tagAriaDescribedby = AriaDescribedBy ?? (Hint != null && !string.IsNullOrWhiteSpace(Identifier) ? $"hint_{Identifier}" : null) ?? string.Empty;
+            if (tagAriaDescribedby != string.Empty)
+            {
+                attributes["aria-describedby"] = tagAriaDescribedby;
+            }
+
+            return attributes;
+        }
+    }
+
+    public class SelectItem
+    {
+        public string Value { get; set; }
+        public MarkupString Description { get; set; }
+        public bool IsDisabled { get; set; }
+    }
+}
