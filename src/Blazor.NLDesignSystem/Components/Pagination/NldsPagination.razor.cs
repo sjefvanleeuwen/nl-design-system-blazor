ba5d6de5dc480f357f1049c5ae7531e93c8b8e57
Marijn Pessers <marijn@pessers.net>
2020-07-15 14:26:10 +0200

    Pagination Working

--- src/Blazor.NLDesignSystem/Components/Pagination/NldsPagination.razor.cs
+++ src/Blazor.NLDesignSystem/Components/Pagination/NldsPagination.razor.cs
@@ -1,37 +1,142 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Blazor.NLDesignSystem.Components.Pagination
 {
     public partial class NldsPagination
     {
-        //[Parameter]
-        //public Action Next { get; set; }
+        [Parameter]
+        public int ActivePage { get; set; } = 1;
+        [Parameter]
+        public Action NextAction { get; set; }
         [Parameter]
         public string NextText { get; set; }
         [Parameter]
-        public Action Previous { get; set; }
+        public int NumberOfPages { get; set; }
+        [Parameter]
+        public int NumberOfPageNumbersToDisplay { get; set; } = 5;
+        [Parameter]
+        public Action<int> PageAction { get; set; }
+        [Parameter]
+        public Action PreviousAction { get; set; }
         [Parameter]
         public string PreviousText { get; set; }
         [Parameter]
-        public string ScreenreaderDescription { get; set; }
+        public string ScreenreaderActivePageDescription { get; set; } = "U bevindt zich op pagina";
+        [Parameter]
+        public string ScreenreaderPageDescription { get; set; } = "pagina";
 
-        //protected bool NextDisabled => Next == null;
-        protected bool PreviousDisabled => Previous == null;
+        private bool NextEnabled => NextAction != null ||
+                                    (PageAction != null && ActivePage < NumberOfPages);
+        private IEnumerable<Page> Pages => BuildDisplayPages();
+        private bool PreviousEnabled => PreviousAction != null ||
+                                        (PageAction != null && ActivePage > 1);
 
         private void InvokeNext()
         {
-            //Next?.Invoke();
-        }
-
-        public void InvokeNextt()
-        {
-            Console.WriteLine("invokecalled");
+            if (NextAction != null)
+            {
+                NextAction.Invoke();
+            }
+            else
+            {
+                InvokePage(ActivePage + 1);
+            }
         }
 
         private void InvokePrevious()
         {
-            Previous?.Invoke();
+            if (PreviousAction != null)
+            {
+                PreviousAction.Invoke();
+            }
+            else
+            {
+                InvokePage(ActivePage - 1);
+            }
         }
+
+        private void InvokePage(int pageNumber)
+        {
+            PageAction?.Invoke(pageNumber);
+        }
+
+        private IEnumerable<Page> BuildDisplayPages()
+        {
+            if (PageAction == null || NumberOfPages < 1)
+            {
+                return new List<Page>();
+            }
+
+            var pageNumbers = DefinePageNumbers();
+            return DefinePagesFromPageNumbers(pageNumbers);
+        }
+
+        private IEnumerable<int> DefinePageNumbers()
+        {
+            var minimumPage = 1;
+            var maximumPage = NumberOfPages;
+
+            var result = new List<int>();
+            if (NumberOfPages > NumberOfPageNumbersToDisplay + 2)
+            {
+                var maxNumbersToLeft = (int)Math.Floor((Decimal)((NumberOfPageNumbersToDisplay - 1) / 2));
+                minimumPage = Math.Max(ActivePage - maxNumbersToLeft, 1);
+                maximumPage = minimumPage + NumberOfPageNumbersToDisplay - 1;
+                if (maximumPage > NumberOfPages)
+                {
+                    minimumPage = NumberOfPages - NumberOfPageNumbersToDisplay + 1;
+                    maximumPage = NumberOfPages;
+                }
+            }
+
+            if (minimumPage > 1)
+            {
+                result.Add(1);
+            }
+            for (var i = minimumPage; i <= maximumPage; i++)
+            {
+                result.Add(i);
+            }
+            if (maximumPage < NumberOfPages)
+            {
+                result.Add(NumberOfPages);
+            }
+
+            return result;
+        }
+
+        private IEnumerable<Page> DefinePagesFromPageNumbers(IEnumerable<int> pageNumbers)
+        {
+            var result = new List<Page>();
+            foreach (var pageNumber in pageNumbers)
+            {
+                var page = new Page { 
+                    Number = pageNumber, 
+                    IsActive = pageNumber == ActivePage
+                };
+                if (pageNumber == 1 && pageNumbers.Count() > 1 && !pageNumbers.Contains(2))
+                {
+                    page.IsFirst = true;
+                }
+                if (pageNumber == pageNumbers.Max() && pageNumbers.Count() > 1 && !pageNumbers.Contains(pageNumber - 1))
+                {
+                    page.IsLast = true;
+                }
+
+                result.Add(page);
+            }
+            return result;
+        }
+    }
+
+    public class Page
+    {
+        public int Number { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsFirst { get; set; }
+        public bool IsLast { get; set; }
     }
 }
