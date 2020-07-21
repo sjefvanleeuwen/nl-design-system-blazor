1fa8cbd5511487d123cbad0f5d5330b6b5c46a32
Marijn Pessers <marijn@pessers.net>
2020-07-21 14:53:00 +0200

    All interop javascript in 1 file

--- src/Blazor.NLDesignSystem/wwwroot/interop/collapse.js
+++
@@ -1,5 +0,0 @@
-﻿function collapse(el) {
-    System.import('_content/Blazor.NLDesignSystem/dist/components/collapse/collapse.js').then(function (module) {
-        new module.Collapse(el);
-    });
-}
--- src/Blazor.NLDesignSystem/wwwroot/interop/interop.js
+++ src/Blazor.NLDesignSystem/wwwroot/interop/interop.js
@@ -1,4 +1,27 @@
-﻿function navigationSubmenu(el) {
+﻿//Combobox
+
+<script>
+  System.import('/uno/components/form/combobox.js').then(function (module) {
+        var combobox = new module.Combobox(document.getElementById('example_combobox'));
+        combobox.allowUnknown = false;
+        combobox.data = data; // your data
+    });
+
+</script>
+
+
+
+//Collapse
+
+function collapse(el) {
+    System.import('_content/Blazor.NLDesignSystem/dist/components/collapse/collapse.js').then(function (module) {
+        new module.Collapse(el);
+    });
+}
+
+//Navigation
+
+function navigationSubmenu(el) {
   System.import('_content/Blazor.NLDesignSystem/dist/components/navigation/navigation.js').then(function (module) {
     new module.SubMenuNavigation(el);
   });
