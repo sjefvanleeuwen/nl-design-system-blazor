cc831bd513de56fbbf6385bc8b5b4d86d9eb12e6
sjefvanleeuwen <sjef.van.leeuwen@live.nl>
2020-07-27 19:28:17 +0200

    Adding Title to Card

--- src/Blazor.NLDesignSystem/Components/Card/NldsCard.razor.cs
+++ src/Blazor.NLDesignSystem/Components/Card/NldsCard.razor.cs
@@ -6,15 +6,15 @@ namespace Blazor.NLDesignSystem.Components
     {
         [CascadingParameter(Name = "Small")]
         public bool Small { get; set; }
-
         [Parameter]
         public string ImageUri { get; set; }
         [Parameter]
         public bool Square { get; set; }
         [Parameter]
         public string Uri { get; set; } = "#";
-
         [Parameter]
-        public RenderFragment ChildContent { get; set; }
+        public RenderFragment Title { get; set; }
+        [Parameter]
+        public RenderFragment Content { get; set; }
     }
 }
